2fac9fe919a2cc43092568aaa6688f3d98a9c62b
Aadithya <aadithyakrishna@gmail.com>
2024-08-31 03:39:59 +0530

    10.5 MiB -- SS

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello, World!")
+import dxcam
+import numpy
+import cv2
+from memory_profiler import profile
+
+
+@profile
+def capture_ss():
+    while True:
+        screen = dxcam.create(output_color="BGR")
+        frame = screen.grab()
+
+        if frame is None:
+            continue
+        frame = cv2.resize(src=frame, dsize=(640, 360))
+
+        print(frame.shape)
+        cv2.imshow("screen", frame)
+        cv2.waitKey(0)
+        break
+
+
+capture_ss()
