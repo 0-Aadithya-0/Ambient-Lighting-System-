683986e6134ad26709d67500a860d8b56fc02051
AAdithya <aadithyakrishna006@gmail.com>
2024-08-31 17:34:14 +0530

    9.7~10 MiB -- SS

--- main.py
+++ main.py
@@ -8,16 +8,12 @@ from memory_profiler import profile
 def capture_ss():
     while True:
         screen = dxcam.create(output_color="BGR")
-        frame = screen.grab()
-
-        if frame is None:
-            continue
-        frame = cv2.resize(src=frame, dsize=(640, 360))
-
-        print(frame.shape)
-        cv2.imshow("screen", frame)
-        cv2.waitKey(0)
+        frame = cv2.resize(src=screen.grab(), dsize=(320, 240))
+        if frame is None: continue
         break
+
+
+
 
 
 capture_ss()
