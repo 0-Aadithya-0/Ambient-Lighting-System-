043de44e47e4a067a047ad6522569e5689c7281b
AAdithya <aadithyakrishna006@gmail.com>
2024-08-31 19:24:09 +0530

    8.8~9 MiB -- SS Color-Theif working

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
+
+import time
 import dxcam
 import numpy
 import cv2
 from memory_profiler import profile
+from colorthief import ColorThief
 
 
 @profile
-def capture_ss():
-    while True:
-        screen = dxcam.create(output_color="BGR")
+def main():
+    def capture_ss():
+
+        screen = dxcam.create(output_color="BGRA")
         frame = cv2.resize(src=screen.grab(), dsize=(320, 240))
-        if frame is None: continue
+        if frame is None: return
+        cv2.imwrite("ss.jpg", frame)
+        time.sleep(0.5)
+
+    def colorfinder():
+
+        ct = ColorThief("ss.jpg")
+        print(ct.get_palette(color_count=4, quality=1))
+
+    while True:
+        capture_ss()
+        colorfinder()
         break
 
 
-
-
-
-capture_ss()
+if __name__ == '__main__':
+    main( )
