701ef4366394572168d402891091e29978d166fd
AAdithya <aadithyakrishna006@gmail.com>
2024-09-08 16:54:26 +0530

    MQTT slow and not stable

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
-
+import sys
 import time
-from typing import List, Any
-
+import paho.mqtt.client as mqtt
 import dxcam
 import numpy as np
 import cv2
+from Tools.scripts.generate_global_objects import Printer
 from memory_profiler import profile
 from colorthief import ColorThief
+Host = "127.0.0.1"
 
 
 @profile
 def main():
 
-    def capture_ss():
+    def SS_Getter():
 
-        screen = dxcam.create(output_color="BGRA")
-        frame = cv2.resize(src=screen.grab(), dsize=(320, 240))
-        if frame is None: return
+        screen = dxcam.create(output_color="BGRA")#BGRA is the output format that consumes the least memory
+        frame = cv2.resize(src=screen.grab(), dsize=(320, 240))#320p
+        #frame = cv2.pyrDown() #downsample 2x
+        if frame is None: return #dxcam returns non if no news frames are available
         frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2RGBA)
         cv2.imwrite("ss.jpg", frame)
-        time.sleep(0.5)
 
-    def colorfinder():
-
-        ct = ColorThief("ss.jpg")
-        color = ct.get_palette(color_count=4,quality=1)
-        print(color)
-
-        def show_palette(rgb_vals,count):
-
-            #Create a blank image of size 800x800 with 3 color channels (BGR)
+    def RGB_Publisher():
+        def colorfinder():
+            ct = ColorThief("ss.jpg")  # gets half the job done
+            color = ct.get_palette(color_count=4, quality=1)
+            return color
+        
+        def show_palette(rgb_vals,count):#(optional)-> fucn to show the colour palette
+            #Cerate a blank image of size 800x800 with 3 color
             image = np.zeros((800, 800, 3), dtype=np.uint8)
             if count == 1 :
                 image[:] =rgb_vals[0]
@@ -50,15 +50,40 @@ def main():
             cv2.waitKey(3000)
             cv2.destroyAllWindows()
 
-        #show_palette(color,1)
+        def tuple_to_str(t):
+            st = str(t)
+            return st
 
+        def mqtt_handler():
+            client = mqtt.Client("RGB_val")
+            while client.connect(Host):
+                print("----------Trying to connect to broker-----------")
+            client.loop_start()
+            client.publish("lights/RGB_val","HEY NIGESH",qos= 1 )
+
+
+
+            #print(f"----------printed value:{payload}-----------")
+
+
+        #show_palette(colorfinder( ), 4)
+        mqtt_handler()
+        #print(colorfinder())
+
+
+
+    
+    
+    
     while True:
-        capture_ss()
-        colorfinder()
+        #SS_Getter()
+        RGB_Publisher()
+        # Remove this break
+        #need to publish continues data to mqtt broker
 
 
 
 
 if __name__ == '__main__':
-    time.sleep(4)
+
     main( )
